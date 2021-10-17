bbf25e2a00d7b1a032020dbf2db6b3244a66bb3d
AlejoThelis <92683599+AlejoThelis@users.noreply.github.com>
2021-10-17 10:11:06 -0600

    Add files via upload

---
+++ Lab 2 - Jesus Alejandro Serrano Thelis/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ffP = "";
+            string rrP = "";
+            string ddR = "";
+
+            Console.WriteLine("Hello, Welcome to the program \"Should the door activate or should it not ??!!!!\" ");
+            Console.WriteLine("True = T , False = F ");
+            Console.WriteLine("Is the Front Pad occupied? (T or F) = ");
+            ffP = Console.ReadLine();
+            string ucaseffP = ffP.ToUpper();
+            Console.WriteLine("Is the Rear Pad occupied? (T or F) = ");
+            rrP = Console.ReadLine();
+            string ucaserrP = rrP.ToUpper();
+            Console.WriteLine("Is the Door Open? (T or F) = ");
+            ddR = Console.ReadLine();
+            string ucaseddR = ddR.ToUpper();
+
+            if (ffP.Equals("t" , StringComparison.OrdinalIgnoreCase))
+            {
+                if (rrP.Equals("t", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (ddR.Equals("t", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("The door will be activated ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The door will no be activated");
+                    }
+                }
+                else
+                {
+                    
+                    if (ddR.Equals("t", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("The door will be activated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The door will be activated");
+                    }
+                }
+            }
+            else
+            {
+                if (rrP.Equals("t", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (ddR.Equals("t", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("The door will be activated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The door will not be activated");
+                    }
+                }
+                else
+                {
+                    if (ddR.Equals("t", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("The door will not be activated");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The door will not be activated");
+                    }
+                }
+            }
+        }
+    }
+}
